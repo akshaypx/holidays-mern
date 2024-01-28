77ade7472d617ffce56b35d000917175a3961050
NAGARRO\akshaysrivastava <akshaysrivastava46@gmail.com>
2024-01-28 12:15:27 +0530

    feat: validate token added

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,9 @@
 import { Link, useNavigate } from "react-router-dom";
 import Button from "./ui/Button";
+import { useAppContext } from "../contexts/AppContext";
 
 const Header = () => {
+  const { isLoggedIn } = useAppContext();
   const navigate = useNavigate();
   return (
     <div className="bg-blue-800 py-6">
@@ -10,9 +12,28 @@ const Header = () => {
           <Link to={"/"}>Holidays.com</Link>
         </span>
         <span className="flex space-x-2">
-          <Button variant="primary" onClick={() => navigate("/sign-in")}>
-            Sign In
-          </Button>
+          {isLoggedIn ? (
+            <>
+              <Button
+                variant="primary"
+                onClick={() => navigate("/my-bookings")}
+              >
+                My Bookings
+              </Button>
+              <Button variant="primary" onClick={() => navigate("/my-hotels")}>
+                My Hotels
+              </Button>
+              <Button variant="primary" onClick={() => {}}>
+                Sign Out
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button variant="primary" onClick={() => navigate("/sign-in")}>
+                Sign In
+              </Button>
+            </>
+          )}
         </span>
       </div>
     </div>
